dcff2b3e33aa59a912c5927d11472cd213753025
artsiomandryianau <artsiom.andryianau@gmail.com>
2020-04-02 22:06:39 +0200

    New task

--- src/Task258AddDigits.java
+++ src/Task258AddDigits.java
@@ -1,4 +1,15 @@
 public class Task258AddDigits {
+    /*
+    Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+             Since 2 has only one digit, return it.
+
+     */
         public static int addDigits(int num) {
             if(num < 10) return num;
 
@@ -16,5 +27,5 @@ public class Task258AddDigits {
             }
             return res;
         }
-    
+
 }
